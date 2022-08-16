4b39c57772e68133d205e7922acce7291a3acae4
zainsubhani <j.zainsubhani786@gmail.com>
2022-08-16 15:46:13 -0700

    jjj

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -119,30 +119,55 @@ const data = [
     des: "Join the Supabase team and get involed  ",
   },
 ];
-
+const productsdata = [
+  {
+    id: 1,
+    icon: <FiDatabase size={20} />,
+    icondes: "Database",
+    des: `Every project is a full Postgres database, the world's most trusted relational database.`,
+  },
+  {
+    id: 2,
+    icon: <FiDatabase size={20} />,
+    icondes: "Authentication",
+    des: `Add user sign ups and logins, securing your data with Row Level Security.`,
+  },
+  {
+    id: 3,
+    icon: <FiDatabase size={20} />,
+    icondes: "Storage",
+    des: `Store, organize, and serve large files. Any media, including videos and images.`,
+  },
+  {
+    id: 4,
+    icon: <FiDatabase size={20} />,
+    icondes: "Edge Functions",
+    des: `Write custom code without deploying or scaling servers.`,
+  },
+];
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Navbar() {
   return (
-    <Popover className="relative ">
+    <Popover className=" ">
       <div
         className="absolute inset-0 shadow z-30 pointer-events-none"
         aria-hidden="true"
       />
       <div className="relative z-20">
-        <div className="w-[1349px] h-[65px] mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
-          <div>
-            <a href="#" className="flex">
-              <img className="lg:h-[50px] w-[124px] " src={LOGO} alt="" />
-            </a>
-          </div>
-          <div className="-mr-2 -my-2 md:hidden">
-            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+        <div className=" h-[65px] mx-auto w-full flex  items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
+          <div className="-mr-2 -my-2 md:hidden block">
+            <Popover.Button className=" rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
               <span className="sr-only">Open menu</span>
               <MenuIcon className="h-6 w-6" aria-hidden="true" />
             </Popover.Button>
+          </div>
+          <div className="md:w-auto w-full flex justify-center">
+            <a href="#" className="flex ">
+              <img className="lg:h-[30px] w-[124px] " src={LOGO} alt="" />
+            </a>
           </div>
           <div className="hidden md:flex-1 md:flex md:items-center md:justify-between">
             <Popover.Group as="nav" className="flex space-x-10">
@@ -304,110 +329,73 @@ export default function Navbar() {
       >
         <Popover.Panel
           focus
-          className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+          className=" absolute border border-gray-700  z-30 top-0 inset-x-0  transition transform origin-top-right md:hidden "
         >
-          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+          <div className=" shadow-xl ring-1 ring-black ring-opacity-5 bg-[#232323]  ">
             <div className="pt-5 pb-6 px-5 sm:pb-8">
               <div className="flex items-center justify-between">
-                <div>
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
-                    alt="Workflow"
-                  />
-                </div>
+                <div></div>
                 <div className="-mr-2">
-                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <Popover.Button className=" inline-flex items-center justify-center text-white hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="sr-only">Close menu</span>
                     <XIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
                 </div>
               </div>
-              <div className="mt-6 sm:mt-8">
-                <nav>
-                  <div className="grid gap-7 sm:grid-cols-2 sm:gap-y-8 sm:gap-x-4">
-                    {/* {solutions.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50"
-                      >
-                        <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                          <item.icon className="h-6 w-6" aria-hidden="true" />
-                        </div>
-                        <div className="ml-4 text-base font-medium text-gray-900">{item.name}</div>
-                      </a>
-                    ))} */}
-                  </div>
-                  <div className="mt-8 text-base">
-                    <a
-                      href="#"
-                      className="font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      {" "}
-                      View all products <span aria-hidden="true">&rarr;</span>
-                    </a>
-                  </div>
-                </nav>
-              </div>
             </div>
-            <div className="py-6 px-5">
-              <div className="grid grid-cols-2 gap-4">
+            <div className=" px-5 pb-10">
+              <div className="grid grid-cols-1 gap-4">
                 <a
                   href="#"
-                  className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
+                  className="rounded-md text-base font-medium text-white"
                 >
-                  Pricing
+                  Sign in
                 </a>
 
                 <a
                   href="#"
-                  className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
+                  className="rounded-md text-base font-medium text-white"
                 >
-                  Docs
+                  Developers
                 </a>
 
                 <a
                   href="#"
-                  className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
+                  className="rounded-md text-base font-medium text-white"
                 >
                   Company
                 </a>
 
                 <a
                   href="#"
-                  className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
+                  className="rounded-md text-base font-medium text-white"
                 >
-                  Resources
+                  Pricing
                 </a>
 
                 <a
                   href="#"
-                  className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
+                  className="rounded-md text-base font-medium text-white"
+                >
+                  GitHub
+                </a>
+
+                <a
+                  href="#"
+                  className="rounded-md text-base font-medium text-white"
                 >
                   Blog
                 </a>
-
-                <a
-                  href="#"
-                  className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
-                >
-                  Contact Sales
-                </a>
               </div>
-              <div className="mt-6">
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                >
-                  Sign up
-                </a>
-                <p className="mt-6 text-center text-base font-medium text-gray-500">
-                  Existing customer?{" "}
-                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
-                    Sign in
-                  </a>
-                </p>
+              <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1  mt-10   gap-10">
+                {productsdata.map((v, k) => (
+                  <Products
+                    key={v.id}
+                    icon={v.icon}
+                    icondes={v.icondes}
+                    des={v.des}
+                  />
+                ))}
               </div>
             </div>
           </div>
